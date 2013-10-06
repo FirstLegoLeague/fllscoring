327bea08616ae3436e1c702ef375b634bf046315
rikkertkoppes <rikkert@rikkertkoppes.com>
2013-10-06 13:31:47 +0200

    included scores locally for now

--- src/js/views/scores.js
+++ src/js/views/scores.js
@@ -5,13 +5,99 @@ define([
 ], function(log, fs) {
     var moduleName = 'scores';
 
+    var field = {
+        "title":"Senior Solutions",
+        "observables":{
+            "pinsDown": {
+                "title":"Number of pins down",
+                "max":6,
+                "type":Number
+            },
+            "weightAtRedMaker": {
+                "title":"Weight height at the red maker",
+                "type":Boolean
+            },
+            "weightAboveRedMaker": {
+                "title":"Weight height above red maker",
+                "type":Boolean
+            },
+            "blueQuilts":{
+                "title":"Blue quilt square touching their black target regions",
+                "max":2,
+                "type":Number
+            },
+            "orangeQuilts":{
+                "title":"Orange quilt square touching their black target regions",
+                "max":2,
+                "type":Number
+            },
+            "chairFixedInBase":{
+                "title":"Chair fixed in base",
+                "type":Boolean
+            },
+            "chairFixedUnderTable":{
+                "title":"Chair fixed and any part under table",
+                "type":Boolean
+            },
+            "orangeBottleMoved":{
+                "title":"Orange bottle moved or outside lines",
+                "type":Boolean
+            },
+            "greenBottleInBase":{
+                "title":"Green bottle in base",
+                "type":Boolean
+            }
+        },
+        "rules": {
+            "bowling":{
+                "title":"Bowling",
+                "exp":function(pinsDown) {
+                    if (pinsDown < 6) {
+                        return pinsDown*7;
+                    }
+                    return 60;
+                }
+            },
+            "strength":{
+                "title":"Strength Exercise",
+                "exp":function(weightAtRedMaker,  weightAboveRedMaker) {
+                    if (weightAtRedMaker && weightAboveRedMaker) {
+                        throw new Error('select only one');
+                    }
+                    return weightAtRedMaker*15 + weightAboveRedMaker*25;
+                }
+            },
+            "quilting":{
+                "title":"Quiting",
+                "exp":function(blueQuilts,orangeQuilts) {
+                    return blueQuilts*15 + orangeQuilts*30;
+                }
+            },
+            "woodworking":{
+                "title":"Wood Working",
+                "exp":function(chairFixedInBase,chairFixedUnderTable) {
+                    if (chairFixedInBase && chairFixedUnderTable) {
+                        throw new Error('select only one');
+                    }
+                    return chairFixedInBase*15 + chairFixedUnderTable*25;
+                }
+            },
+            "medicin":{
+                "title":"Medicine",
+                "exp":function(orangeBottleMoved,greenBottleInBase) {
+                    return 25*(!orangeBottleMoved && greenBottleInBase);
+                }
+            }
+        }
+    };
+
     angular.module(moduleName, []).controller(moduleName + 'Ctrl', [
         '$scope',
         function($scope) {
             log('init scores ctrl');
 
-            fs.read('field.js').then(function(field) {
-                var field = eval('('+field+')');
+            // fs.read('field.js').then(function(defs) {
+                // var field = eval('('+defs+')');
                 $scope.observables = field.observables;
                 $scope.rules = field.rules;
                 angular.forEach($scope.rules,process);
@@ -19,9 +105,9 @@ define([
                 // console.log(test);
             // }).fail(function() {
             //     log('error getting field');
-            }).then(function() {
-                $scope.$apply();
-            });
+            // }).then(function() {
+            //     $scope.$apply();
+            // });
 
             function process(rule,key) {
                 var exp = rule.exp;
