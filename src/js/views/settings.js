a8404f7867be0b0120ed1e238bafe1823fdcd4d7
rikkertkoppes <rikkert@rikkertkoppes.com>
2013-10-17 14:36:14 +0200

    Added basic settings for table and challenge

--- src/js/views/settings.js
+++ src/js/views/settings.js
@@ -1,15 +1,28 @@
 define([
     'services/log',
+    'services/ng-fs',
     'angular'
 ],function(log) {
     var moduleName = 'settings';
     return angular.module(moduleName,[]).controller(moduleName+'Ctrl',[
-        '$scope',
-        function($scope) {
+        '$scope', '$fs',
+        function($scope,$fs) {
             log('init settings ctrl');
             $scope.log = log.get();
 
+            $fs.read('settings.json').then(function(res) {
+                $scope.settings = res;
+            },function() {
+                $scope.settings = {};
+            });
 
+            $scope.save = function() {
+                $fs.write('settings.json',$scope.settings).then(function() {
+                    log('settings saved');
+                },function() {
+                    log('unable to write settings');
+                });
+            };
         }
     ]);
 });
