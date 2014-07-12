1410b91ab42c472343d5cc0245d4e409b3bc4362
rikkertkoppes <rikkert@rikkertkoppes.com>
2014-07-12 10:06:18 +0200

    rewritten nwSpec

--- spec/fs-nwSpec.js
+++ spec/fs-nwSpec.js
@@ -1,42 +1,42 @@
-define(['injector'],function(Injector) {
-    var injector = new Injector();
+describe('fs-nw',function() {
+    var fs = factory('services/fs-nw',{
+        'q':Q,
+        'idbstore': IDBStore
+    });
+    describe('test',function() {
+        it('should pass',function() {
+            expect(true).toBe(true);
+        });
+    });
 
-    return injector.require(['services/fs-nw'],function(fs) {
-        describe('test',function() {
-            it('should pass',function() {
-                expect(true).toBe(true);
+    describe('fs signature',function() {
+        it('should have a read method',function() {
+            expect(fs.read).not.toBeUndefined();
+        });
+    });
+
+    describe('reading',function() {
+        it('should read a test file',function(done) {
+            fs.write('foo/foo.txt','jsfd978sd').then(function() {
+                return fs.read('foo/foo.txt');
+            }).then(function(data) {
+                expect(data).toBe('jsfd978sd');
+            }).fail(function(data) {
+                console.log('fail',data);
+            }).then(function() {
+                fs.remove('foo/foo.txt').then(done);
             });
         });
+    });
 
-        describe('fs signature',function() {
-            it('should have a read method',function() {
-                expect(fs.read).not.toBeUndefined();
-            });
-        });
-
-        describe('reading',function() {
-            it('should read a test file',function(done) {
-                fs.write('foo/foo.txt','jsfd978sd').then(function() {
-                    return fs.read('foo/foo.txt');
-                }).then(function(data) {
-                    expect(data).toBe('jsfd978sd');
-                }).fail(function(data) {
-                    console.log('fail',data);
-                }).then(function() {
-                    fs.remove('foo/foo.txt').then(done);
-                });
-            });
-        });
-
-        describe('writing',function() {
-            it('should write a test file',function(done) {
-                fs.write('foo/foo.txt','jsfd978sd').then(function(data) {
-                    expect(data).toBe('foo/foo.txt');
-                }).fail(function(data) {
-                    console.log('fail',data);
-                }).then(function() {
-                    fs.remove('foo/foo.txt').then(done);
-                });
+    describe('writing',function() {
+        it('should write a test file',function(done) {
+            fs.write('foo/foo.txt','jsfd978sd').then(function(data) {
+                expect(data).toBe('foo/foo.txt');
+            }).fail(function(data) {
+                console.log('fail',data);
+            }).then(function() {
+                fs.remove('foo/foo.txt').then(done);
             });
         });
     });
