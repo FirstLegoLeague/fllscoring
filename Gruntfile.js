34d2409c3a1f65272ea6eb70b6e40e958f6f4f13
rikkertkoppes <rikkert@rikkertkoppes.com>
2014-10-25 20:00:44 +0200

    Added transformation to PDF task (grunt pdf) using phantomJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,34 @@ module.exports = function(grunt) {
                 src: 'challenges/xml/*.xml',
                 dest: 'challenges/html/'
             }
+        },
+
+        'http-server': {
+            'dev': {
+                root: "challenges/html/",
+                port: 8282,
+                host: "127.0.0.1",
+                // cache: < sec > ,
+                // showDir: true,
+                // autoIndex: true,
+
+                // server default file extension
+                ext: "html",
+
+                // run in parallel with other tasks
+                runInBackground: true
+            }
+        },
+
+        phantomJSScreenShot: {
+            options: {
+                phantomPath: 'node_modules/.bin/phantomjs',
+                address: "http://127.0.0.1:8282/"
+            },
+            files: {
+                src: 'challenges/html/*.html',
+                dest: 'challenges/pdf/'
+            }
         }
     });
 
@@ -81,8 +109,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-phonegap-build');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-http-server');
 
     grunt.registerTask('phonegap', ['compress', 'phonegap-build:debug']);
+    grunt.registerTask('html', ['saxon']);
+    grunt.registerTask('pdf', ['saxon','http-server', 'phantomJSScreenShot']);
 
     grunt.registerMultiTask('saxon', function() {
         var options = this.options();
@@ -91,7 +122,9 @@ module.exports = function(grunt) {
         var npath = require('path');
         var nfs = require('fs');
         var saxon = require('saxon-stream2');
-        var xslt = saxon(saxonPath,xslPath,{timeout:5000});
+        var xslt = saxon(saxonPath, xslPath, {
+            timeout: 5000
+        });
         var done = this.async();
 
         this.files.forEach(function(f) {
@@ -104,20 +137,62 @@ module.exports = function(grunt) {
                     return true;
                 }
             }).forEach(function(filepath) {
-                var base = npath.basename(filepath,'.xml');
-                var dest = npath.resolve(f.dest,base)+'.html';
+                var base = npath.basename(filepath, '.xml');
+                var dest = npath.resolve(f.dest, base) + '.html';
                 // console.log(filepath);
                 // console.log(dest);
-                nfs.createReadStream(filepath,{encoding:'utf-8'})
+                nfs.createReadStream(filepath, {
+                    encoding: 'utf-8'
+                })
                     .pipe(xslt)
                     .pipe(nfs.createWriteStream(dest))
-                    .on('error',function(err) {
+                    .on('error', function(err) {
                         done(err);
                     })
-                    .on('finish',function() {
+                    .on('finish', function() {
                         console.log('done');
                         done();
                     });
+            });
+        });
+    });
+
+    grunt.registerMultiTask('phantomJSScreenShot', function() {
+        var options = this.options();
+        var phantomPath = options.phantomPath;
+        var address = options.address;
+        var npath = require('path');
+        var nfs = require('fs');
+        var done = this.async();
+        var exec = require('child_process').exec;
+
+        this.files.forEach(function(f) {
+            f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).forEach(function(filepath) {
+                var base = npath.basename(filepath, '.html');
+                var dest = npath.resolve(f.dest, base) + '.pdf';
+
+                var cmd = [
+                    '"' + phantomPath + '"',
+                    '"tools/rasterize.js"',
+                    '"' + address + base + '.html"',
+                    '"' + dest + '"',
+                    'a4'
+                ].join(' ');
+
+                exec(cmd, function(error, stdout, stderr) {
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                    done();
+                });
             });
         });
     });
