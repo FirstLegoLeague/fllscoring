951ee71ab8af2a59efe971c4d18de74ded8c70e0
rikkertkoppes <rikkert@rikkertkoppes.com>
2014-11-16 22:02:45 +0100

    Do not run coverage locally as default, since it messes up line numbers

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress','coverage'],
+    reporters: ['progress'],
 
     preprocessors: {
       // source files, that you wanna generate coverage for
