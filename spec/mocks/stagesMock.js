01533c8b987b28e494cf495eaf813d3db603f7c5
Martin Poelstra <martin@beryllium.net>
2014-11-23 17:31:56 +0100

    stagesMock: Add get() method, update $rounds property.

--- spec/mocks/stagesMock.js
+++ spec/mocks/stagesMock.js
@@ -1,9 +1,19 @@
 var stagesMock = {
     stages: [
-        { id: "practice", name: "Oefenrondes", rounds: 2, _rounds: [1, 2] },
-        { id: "qualifying", name: "Voorrondes", rounds: 3, _rounds: [1, 2, 3] },
-        { id: "quarter", name: "Kwart finales", rounds: 0, _rounds: [] },
-        { id: "semi", name: "Halve finales", rounds: 0, _rounds: [] },
-        { id: "final", name: "Finale", rounds: 1, _rounds: [1] },
-    ]
+        { id: "practice", name: "Oefenrondes", rounds: 2, $rounds: [1, 2] },
+        { id: "qualifying", name: "Voorrondes", rounds: 3, $rounds: [1, 2, 3] },
+        { id: "quarter", name: "Kwart finales", rounds: 0, $rounds: [] },
+        { id: "semi", name: "Halve finales", rounds: 0, $rounds: [] },
+        { id: "final", name: "Finale", rounds: 1, $rounds: [1] },
+    ],
+    get: function(id) {
+        var stages = stagesMock.stages;
+        var i;
+        for (i = 0; i < stages.length; i++) {
+            if (stages[i].id === id) {
+                return stages[i];
+            }
+        }
+        throw new Error("unknown stage");
+    }
 }
