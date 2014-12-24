01f432fea3e5a66eca52b2eeb004717f5ca9ac45
rikkertkoppes <rikkert@rikkertkoppes.com>
2014-12-24 18:21:24 +0100

    updated xhrSpec

--- spec/services/fs-xhrSpec.js
+++ spec/services/fs-xhrSpec.js
@@ -1,36 +1,24 @@
 describe('fs-xhr',function() {
 
+    function createJQPromise(success) {
+        return {
+            done: function() {
+                var p = Q.when(success);
+                return p.then.apply(p,arguments);
+            }
+        };
+    }
+
+    var fakeJQuery = {
+        get: jasmine.createSpy('jqget').andReturn(createJQPromise('get')),
+        post: jasmine.createSpy('jqpost').andReturn(createJQPromise('post')),
+        ajax: jasmine.createSpy('jqajax').andReturn(createJQPromise('ajax'))
+    };
+
     var fs = factory('services/fs-xhr',{
-        'q':Q
+        'q':Q,
+        'jquery': fakeJQuery
     });
-
-    //mock xhr get
-    fs._get = function(url) {
-        return {
-            done: function(cb) {
-                cb(url);
-                return {
-                    fail: function(cb) {
-                        //nothing
-                    }
-                };
-            }
-        };
-    };
-
-    //mock xhr post
-    fs._post = function(url,data) {
-        return {
-            done: function(cb) {
-                cb(url);
-                return {
-                    fail: function(cb) {
-                        //nothing
-                    }
-                };
-            }
-        };
-    };
 
     describe('fs signature',function() {
         it('should have a read method',function() {
@@ -45,26 +33,56 @@ describe('fs-xhr',function() {
     });
 
     describe('reading',function() {
-        it('should read a test file',function(done) {
-            fs.read('foo.txt').then(function(data) {
-                expect(data).toBe('/fs/foo.txt');
-            }).fail(function(data) {
-                console.log('fail',data);
-            }).then(function() {
-                done();
+        it('should read a test file',function() {
+            return fs.read('foo.txt').then(function(data) {
+                expect(data).toBe('get');
+                expect(fakeJQuery.get).toHaveBeenCalledWith('/fs/foo.txt');
+            });
+        });
+        it('should fail on xhr exception',function() {
+            fakeJQuery.get.andReturn(createJQPromise(Q.reject(new Error('foo'))));
+            return fs.read('foo.txt').catch(function(err) {
+                expect(fakeJQuery.get).toHaveBeenCalledWith('/fs/foo.txt');
+                expect(err.message).toBe('foo');
             });
         });
     });
 
     describe('writing',function() {
-        it('should write a test file',function(done) {
-            fs.write('foo.txt').then(function(data) {
-                expect(data).toBe('/fs/foo.txt');
-            }).fail(function(data) {
-                console.log('fail',data);
-            }).then(function() {
-                done();
+        it('should write a test file',function() {
+            return fs.write('foo.txt','bar').then(function(data) {
+                expect(fakeJQuery.post).toHaveBeenCalledWith('/fs/foo.txt','bar');
+                expect(data).toBe('post');
+            });
+        });
+        it('should fail on xhr exception',function() {
+            fakeJQuery.post.andReturn(createJQPromise(Q.reject(new Error('foo'))));
+            return fs.write('foo.txt','bar').catch(function(err) {
+                expect(fakeJQuery.post).toHaveBeenCalledWith('/fs/foo.txt','bar');
+                expect(err.message).toBe('foo');
             });
         });
     });
+
+    describe('removing',function() {
+        it('should remove a test file',function() {
+            return fs.remove('foo.txt').then(function(data) {
+                expect(fakeJQuery.ajax).toHaveBeenCalledWith('/fs/foo.txt',{
+                    type: 'DELETE'
+                });
+                expect(data).toBe('ajax');
+            });
+        });
+        it('should fail on xhr exception',function() {
+            fakeJQuery.ajax.andReturn(createJQPromise(Q.reject(new Error('foo'))));
+            return fs.remove('foo.txt','bar').catch(function(err) {
+                expect(fakeJQuery.ajax).toHaveBeenCalledWith('/fs/foo.txt',{
+                    type: 'DELETE'
+                });
+                expect(err.message).toBe('foo');
+            });
+        });
+    });
+
+
 });
