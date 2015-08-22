fceadd2f4210edd947b2f057e240f4d4d1bd106b
Jeroen <bosch.jeroen@gmail.com>
2015-08-22 22:22:14 +0200

    add export round results

--- src/js/views/ranking.js
+++ src/js/views/ranking.js
@@ -7,15 +7,20 @@ define('views/ranking',[
 ],function(log) {
     var moduleName = 'ranking';
     return angular.module(moduleName,[]).controller(moduleName+'Ctrl', [
-        '$scope', '$scores', '$stages',
-        function($scope, $scores, $stages) {
+        '$scope', '$scores', '$stages', '$timeout',
+        function($scope, $scores, $stages, $timeout) {
             log('init ranking ctrl');
 
             // temporary default sort values
             $scope.sort = 'rank';
             $scope.rev = false;
+            $scope.export = {};
+            $scope.export.prevRounds = true; // enable highscore
             // initialize first tab
             $scope.tab = 1;
+            $scope.scores = $scores;
+
+
 
             $scope.doSort = function(stage, col, defaultSort) {
                 if (stage.sort === undefined) {
@@ -116,6 +121,26 @@ define('views/ranking',[
 
             $scope.stages = $stages.stages;
             $scope.scoreboard = $scores.scoreboard;
+
+            $scope.getRoundLabel = function(round){
+                return "Round " + round;
+            };
+            // Generate new table and download page to wished location
+            $scope.exportScore = function(params){
+                $scope.stageselected = params.stage;
+
+                $timeout(function () {
+
+                    var htmloutput = "<!DOCTYPE html><html><head><title>"+ params.stage.name + " " + params.round + "</title></head><body>";
+                    htmloutput += document.getElementById("scoreexport").outerHTML;
+                    htmloutput += "</body></html>";
+                    $scope.exportname = encodeURIComponent("RoundResults.html");
+                    $scope.exportdata = "data:text/csv;charset=utf-8," + encodeURIComponent(htmloutput);
+                    $scope.exportvisible = true; 
+
+                });
+            };
+
         }
     ]);
 });
