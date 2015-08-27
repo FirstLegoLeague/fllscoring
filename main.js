718c06437b46dcd97a6e921f0068e2ec27a7f8fb
Jeroen Bosch <bosch.jeroen@gmail.com>
2015-08-27 19:02:24 +0200

    add welcome pop-up

--- main.js
+++ main.js
@@ -12,5 +12,17 @@ function closeQr() {
     qr.classList.remove('visible');
 }
 
+function showDisclaimer(){
+	disclaimer.classList.add('visible');
+}
+
+function acceptDisclaimer(){
+	disclaimer.classList.remove('visible');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	console.log("show me");
+   showDisclaimer()
+}, false);
 /* hide phone browser address bar */
 window.scrollTo(0,1);
