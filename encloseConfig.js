057bc9cbbf1dd5f71b00ee053ea0548266f56814
Kenny meesters <k.meesters@gmail.com>
2016-10-19 20:50:11 +0200

    Config for encloseJS

---
+++ encloseConfig.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  scripts: "./src/**/*.js",
+  assets: ["./src/**/*",
+           "./challenges/**/*"],
+  dirs:  ["./src",
+          "./src/data",
+          "./challenges"]
+
+};
