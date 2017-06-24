73b7ebcf648c89731a1d5bb849a4594cb19e6d88
Idan Stark <idanstark42@gmail.com>
2017-06-24 22:28:51 +0300

    closed the first part of the refactoring

--- server_modules/views.js
+++ server_modules/views.js
@@ -3,13 +3,13 @@ var fileSystem = require('./file_system');
 exports.route = function(app) {
 	
 	app.get('/',function(req,res) {
-		req.session.username = 'user';
+		req.session.user = 'user';
 		console.log(Object.keys(req.session));
 	    res.sendFile(fileSystem.resolve('/src/index.html'));
 	});
 
 	app.get('/admin',function(req,res) {
-	    req.session.username = 'admin';
+	    req.session.user = 'admin';
 	    console.log(Object.keys(req.session));
 	    res.sendFile(fileSystem.resolve('/src/index.html'));
 	});
