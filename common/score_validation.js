b4cf42eab355c461526dd98690b373d3a3277aa9
Idan Stark <idanstark42@gmail.com>
2017-08-07 16:53:08 +0300

    Added scores to commong area

---
+++ common/score_validation.js
@@ -0,0 +1,59 @@
+/* Validation error classes */
+
+const VALIDATORS = [{
+    validate: (score, stages) => !stages.hasOwnProperty(score.stageId),
+    error:(score) => {
+        return {
+            name: 'UnknownStageError',
+            stageId: score.stageId,
+            message: `unknown stage '${String(this.stageId)}'`
+        };
+    }, {
+        validate: (score, stages) => score.round >= 1 && score.round <= stages[score.stageId].rounds,
+        error: (score) => {
+            name: 'UnknownRoundError',
+            round: score.round,
+            message: `unknown round '${String(this.round)}'`
+        }
+    }, {
+        validate: (score) => typeof score.score === "undefined" ||
+                typeof score.score === "number" && score.score > -Infinity && score.score < Infinity,
+        error: (score) => {
+            name: 'InvalidScoreError',
+            score: score.score,
+            message: `invalid score '${String(score)}'`
+    }, {
+        validate: (score, stages, teams) => teams.filter((team) => team.number === score.team.number).length === 1,
+        error: (score) => {
+            name: 'UnknownTeamError',
+            team: score.team,
+            message: `invalid team '${String(this.team)}'`
+    }, {
+        validate: (score, stages, teams, scores) => scores.filter((s) => s.team === score.team && s.stageId === score.stageId && s.round === score.roun).length === 1,
+        error: (score, stages) => {
+            name: 'DuplicateScoreError',
+            team: score.team,
+            stage: stages[score.stageId],
+            round: score.round
+            message: `duplicate score for team '${this.team.name}' (${String(this.team.number)}), stage ${this.stage.name}, round ${this.round}`
+    }
+}];
+
+function ScoreValidator() {
+
+    this.validate = function(scores, stages, teams) {
+        scores.forEach(function(score) {
+            validators: for(var i = 0; i < VALIDATORS.legnth; i++) {
+                var validator = VALIDATORS[i]
+                if(!validator.validate(score, stages, teams, scores)) {
+                    score.error = validator.error(score, stages, teams, scores);
+                    break validators;
+                }
+            }
+        });
+        return scores;
+    };
+
+}
+
+ScoresValidator.deps = ['score'];
---
+++ common/scores.js
@@ -0,0 +1,30 @@
+function Scores(CRUDInterface, teams, stages, messenger) {
+
+    var self = this;
+
+    self.validator = new ScoreValidator();
+
+    CRUDInterface.load().then(setScores);
+
+    this.add = function(score) {
+        return CRUDInterface.add(score).then(setScores);
+    };
+
+    this.delete = function(scoreId) {
+        CRUDInterface.delete(scoreId).then(setScores);
+    };
+
+    this.update = function(score) {
+        CRUDInterface.update(score).then(setScores);
+    }
+
+    function setScores(scores) {
+        if(!scores) return;
+        self.scores = scores.map(score => new Score(score));
+        self.validator.validate(self.scores, team, stages);
+        self.rankings = new Rankings().calculate(scores, stages, teams)
+    }
+
+}
+
+Scores.deps = ['score', 'score_validation', 'rankings'];
