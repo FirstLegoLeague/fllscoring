0e58cb15e485940ec10886f4b6d0e2be37ec3831
Idan Stark <idanstark42@gmail.com>
2017-08-20 23:28:50 +0300

    No comma

--- karma.conf.js
+++ karma.conf.js
@@ -87,7 +87,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
-
+    singleRun: false
   });
 };
