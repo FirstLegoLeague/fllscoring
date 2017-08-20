d93943dc0fc9dea7ff9ff99ea6faf0d083391e07
ThinkRedstone <ittai11@gmail.com>
2017-08-20 19:01:37 +0300

    address.js now only finds and saves one address

--- server_modules/address.js
+++ server_modules/address.js
@@ -3,17 +3,18 @@ var args = require('./args')
 
 //shamelessly copied from StackOverflow with only minor changes
 var interfaces = os.networkInterfaces();
-var addresses = [];
+var serverAddress = "";
 for (var i in interfaces) {
     for (var j in interfaces[i]) {
         var address = interfaces[i][j];//we basically parse over all the addresses for every network interface
-        if (address.family === 'IPv4' && !address.internal) {//if that address is an IPv4 address, and it is not an internal one, we add it
-            addresses.push(address.address);
+        if (address.family === 'IPv4' && !address.internal) {//if that address is an IPv4 address, and it is not an internal one, that's the address we are looking for
+            serverAddress = address.address;
+            break;
         }
     }
 }
 
 exports.middleware = function (req, res, next) {
-    req.session.serverAddress = addresses[0] + ':' + args.port; //we set the server address to the first one. In reality, any of them would work
+    req.session.serverAddress = serverAddress + ':' + args.port;
     next();
 };
