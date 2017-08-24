ef8f73a6b4b64159351fb87072f7b6dfb6d3362c
Idan Stark <idanstark42@gmail.com>
2017-08-25 00:21:15 +0300

    Added specs for the  factory

--- src/js/services/ng-score.js
+++ src/js/services/ng-score.js
@@ -11,43 +11,11 @@ define('services/ng-score',[
     return module.factory('$score',
         [function() {
 
-
-            // Calculating the unique ID for this sanitized score.
-            // The uid is an 8-hex-digit combination of
-            // The current date and a random number.
-            function generateUniqueId() {
-                //The max uid in the range
-                let max = 0x100000000;
-                // The numeric form of the uid
-                let num = (Math.floor(Math.random() * max) + Date.now()) % max;
-                // The string uid, by adding the max value and then removing it we make sure the stringification of the number
-                return (num + max).toString(16).slice(1);
-            }
-
-            // This function is meant to make sure the score is set according to
-            // the correct structure in order to save it.
-            // score that doesn't match the fields in this function,
-            // cannot be saved in the scores summery.
-            function sanitize(entry) {
-                return {
-                    id: entry.id || generateUniqueId(),
-                    file: Boolean(entry.file) ? String(entry.file) : '',
-                    teamNumber: Number(entry.teamNumber || (entry.team ? entry.team.number : 0)),
-                    stageId: String(entry.stageId || (entry.stage ? entry.stage.id : '')),
-                    round: Number(entry.round),
-                    score: isFinite(entry.score) ? Number(entry.score) : undefined,
-                    originalScore: Number(entry.originalScore || entry.score),
-                    edited: Boolean(entry.edited) ? String(entry.edited) : undefined, // timestamp, e.g. "Wed Nov 26 2014 21:11:43 GMT+0100 (CET)"
-                    published: Boolean(entry.published),
-                    table: entry.table
-                };
-            }
-
             function Score(entry) {
 
                 //Adding the data from the entry, snitizing it if needed.
                 (function(score, entry) {
-                    let sanitized = sanitize(entry);
+                    let sanitized = Score.sanitize(entry);
                     for(var key in sanitized) {
                         score[key] = sanitized[key];
                     }
@@ -69,7 +37,40 @@ define('services/ng-score',[
 
             Score.compare = function(score1, score2) {
                 return score2.score - score1.score;
-            }
+            };
+
+            // These two functions has no reason to be public except for UT.
+
+            // Calculating the unique ID for this sanitized score.
+            // The uid is an 8-hex-digit combination of
+            // The current date and a random number.
+            Score.generateUniqueId = function() {
+                //The max uid in the range
+                let max = 0x100000000;
+                // The numeric form of the uid
+                let num = (Math.floor(Math.random() * max) + Date.now()) % max;
+                // The string uid, by adding the max value and then removing it we make sure the stringification of the number
+                return (num + max).toString(16).slice(1);
+            };
+
+            // This function is meant to make sure the score is set according to
+            // the correct structure in order to save it.
+            // score that doesn't match the fields in this function,
+            // cannot be saved in the scores summery.
+            Score.sanitize = function(entry) {
+                return {
+                    id: entry.id || Score.generateUniqueId(),
+                    file: Boolean(entry.file) ? String(entry.file) : '',
+                    teamNumber: Number(entry.teamNumber || (entry.team ? entry.team.number : 0)),
+                    stageId: String(entry.stageId || (entry.stage ? entry.stage.id : '')),
+                    round: Number(entry.round),
+                    score: isFinite(entry.score) ? Number(entry.score) : undefined,
+                    originalScore: Number(entry.originalScore || entry.score),
+                    edited: Boolean(entry.edited) ? String(entry.edited) : undefined, // timestamp, e.g. "Wed Nov 26 2014 21:11:43 GMT+0100 (CET)"
+                    table: entry.table,
+                    published: Boolean(entry.published)
+                };
+            };
 
             return Score;
     }]);
