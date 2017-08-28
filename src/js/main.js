FirstLegoLeague/fllscoring

commit 73b7ebcf648c89731a1d5bb849a4594cb19e6d88

closed the first part of the refactoring

src/js/main.js
@@ -1,5 +1,6 @@
 define([
     'services/log',
+    'services/session',
     'views/settings',
     'views/teams',
     'views/scoresheet',
@@ -26,15 +27,31 @@ define([
     //initialize main controller and load main view
     //load other main views to create dynamic views for different device layouts
     angular.module('main',[]).controller('mainCtrl',[
-        '$scope','$window',
-        function($scope, $window) {
+        '$scope', 'session',
+        function($scope, session) {
             log('init main ctrl');
-            $scope.drawer = 'views/drowar';
-            $scope.pages = ['teams','scoresheet','scores','ranking','settings'];
+            $scope.drawer = 'views/drawer.html';
             $scope.scoringPages = ['scoresheet','settings'];
-            $scope.currentPage = $scope.pages[1];
             $scope.validationErrors = [];
             $scope.drawerVisible = false;
+
+            $scope.user = session.get('user');
+            if($scope.user === 'admin') {
+                $scope.pages = [
+                    { name: 'scoresheet', title: 'Scoresheet', icon: 'check' },
+                    { name: 'teams', title: 'Teams', icon: 'people' },
+                    { name: 'scores', title: 'Scorekeeping', icon: 'list' },
+                    { name: 'ranking', title: 'Ranking', icon: 'format_list_numbered' },
+                    { name: 'settings', title: 'Settings', icon: 'settings' }
+                ];
+            } else {
+                $scope.pages = [
+                    { name: 'scoresheet', title: 'Scoresheet', icon: 'check' },
+                    { name: 'settings', title: 'Settings', icon: 'settings' }
+                ];
+            }
+
+            $scope.currentPage = $scope.pages[0];
 
             $scope.$on('validationError',function(e,validationErrors) {
                 $scope.validationErrors = validationErrors;

server_modules/views.js
@@ -3,13 +3,13 @@ var fileSystem = require('./file_system');
 exports.route = function(app) {
 	
 	app.get('/',function(req,res) {
-		req.session.username = 'user';
+		req.session.user = 'user';
 		console.log(Object.keys(req.session));
 	    res.sendFile(fileSystem.resolve('/src/index.html'));
 	});
 
 	app.get('/admin',function(req,res) {
-	    req.session.username = 'admin';
+	    req.session.user = 'admin';
 	    console.log(Object.keys(req.session));
 	    res.sendFile(fileSystem.resolve('/src/index.html'));
 	});

src/admin.html
@@ -1,24 +1,0 @@
-<!DOCTYPE html>
-<html manifest="fllscoring.appcache">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, target-densitydpi=device-dpi" />
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <link rel="stylesheet" type="text/css" href="components/bootstrap-css/css/bootstrap.min.css">
-        <link href="css/materialIcons.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/main.css">
-        <title>FLL Scoring</title>
-    </head>
-    <body>
-        <script src="js/config.js"></script>
-        <script>
-            require.map = {
-                '*': {
-                    'services/fs':'services/fs-xhr'
-                }
-            };
-        </script>
-        <script src="components/requirejs/require.js" data-main="main"></script>
-        <div ng-controller="mainCtrl" ng-include="mainView" ng-init="setPlatform('nodejs')"></div>
-    </body>
-</html>

src/index.html
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, target-densitydpi=device-dpi" />
-        <meta name="apple-mobile-web-app-capable" content="yes">
         <link rel="stylesheet" type="text/css" href="components/bootstrap-css/css/bootstrap.min.css">
         <link href="css/materialIcons.css" rel="stylesheet">
         <link rel="stylesheet" href="css/main.css">
@@ -23,10 +22,10 @@
             <div fll-size ng-class="containerClass($width,$height)">
                 <div class="drawerShim" ng-class="{visible:drawerVisible}" ng-click="toggleDrawer(false)"></div>
                 <div ng-include="drawer"></div>
-                <div class="viewMain" ng-class="currentPage" ng-cloak>
+                <div class="viewMain" ng-class="currentPage.name" ng-cloak>
                     <div ng-repeat="page in pages"
-                        ng-include="'views/pages/'+page+'.html'"
-                        ng-class="'view-'+page"
+                        ng-include="'views/pages/' + page.name + '.html'"
+                        ng-class="'view-' + page.name"
                     ></div>
                 </div>
                 <div ng-include="'views/dialogs.html'"></div>

src/nwindex.html
@@ -1,24 +1,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, target-densitydpi=device-dpi" />
-        <link rel="stylesheet" type="text/css" href="components/bootstrap-css/css/bootstrap.min.css">
-        <link href="css/materialIcons.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/main.css">
-        <title>FLL Scoring</title>
-    </head>
-    <body>
-        <script src="js/config.js"></script>
-        <script>
-            require.map = {
-                '*': {
-                    'services/fs':'services/fs-nw'
-                }
-            };
-
-        </script>
-        <script src="components/requirejs/require.js" data-main="main"></script>
-        <div ng-controller="mainCtrl" ng-include="mainView" ng-init="setPlatform('nodewebkit')"></div>
-    </body>
-</html>

src/views/drawer.html
@@ -1,0 +1,9 @@
+<div class="drawer" ng-class="{visible:drawerVisible}">
+    <div class="drawerLead">
+    </div>
+    <ul class="nav">
+        <li ng-repeat="page in pages" ng-class="{active: currentPage === page}">
+            <a ng-click="setPage(page)"><i class="material-icons">{{page.icon}}</i> <span>{{page.title}}</span></a>
+        </li>
+    </ul>
+</div>

commit 0acfbb9d4fab4ef1dbbaa51513535e7c0dcf10e1

Fix scores/rankings etc not showing anymore. Broken in 73b7ebcf648c89731a1d5bb849a4594cb19e6d88.

src/js/main.js
@@ -17,7 +17,7 @@ define([
     'angular-touch',
     'angular-sanitize',
     'angular'
-],function(log,settings,teams,scoresheet,scores,ranking,services,directives,size,filters,indexFilter,fsTest,dbTest) {
+],function(log,session,settings,teams,scoresheet,scores,ranking,services,directives,size,filters,indexFilter,fsTest,dbTest) {
 
     log('device ready');
 