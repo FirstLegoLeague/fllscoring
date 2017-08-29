2067f802f0c23bd497e31c695d7d81294520e516
ThinkRedstone <ittai11@gmail.com>
2017-08-29 16:23:36 +0300

    updated publishScore and unpublishScore test suits

--- spec/views/scoresSpec.js
+++ spec/views/scoresSpec.js
@@ -72,18 +72,22 @@ describe('scores', function() {
     });
 
     describe('publishScore',function() {
-        it('should publish a score and save it',function() {
+        it('should update the score with published as true, and the correct wasPublished value',function() {
             var id = 'afg1jkhg';
-            $scope.publishScore({ id: id });
-            expect(scoresMock.update).toHaveBeenCalledWith({ id: id, published: true});
+            $scope.publishScore({ id: id , published: false});
+            expect(scoresMock.update).toHaveBeenCalledWith({ id: id, published: true, wasPublished: false});
+            $scope.publishScore({id: id, published: true});
+            expect(scoresMock.update).toHaveBeenCalledWith({id: id, published: true, wasPublished: true});
         });
     });
 
     describe('unpublishScore',function() {
-        it('should unpublish a score and save it',function() {
+        it('should update the score with published as false, and the correct wasPublished value',function() {
             var id = 'afg1jkhg';
-            $scope.unpublishScore({ id: id });
-            expect(scoresMock.update).toHaveBeenCalledWith({ id: id, published: false });
+            $scope.unpublishScore({ id: id , published: false});
+            expect(scoresMock.update).toHaveBeenCalledWith({ id: id, published: false, wasPublished: false});
+            $scope.unpublishScore({id: id, published: true});
+            expect(scoresMock.update).toHaveBeenCalledWith({id: id, published: false, wasPublished: true});
         });
     });
 
