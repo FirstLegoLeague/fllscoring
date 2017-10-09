8321c6508cbba4cbccca3c620cd34f17b8d30b03
Martin Poelstra <martin@beryllium.net>
2017-10-09 17:00:46 +0200

    file_system: Add hook mechanism for transforming file operations.

--- server_modules/file_system.js
+++ server_modules/file_system.js
@@ -25,6 +25,112 @@ exports.resolve = function(file) {
     return path.resolve(args.rootdir, file);
 };
 
+/**
+ * @typedef {Object} Hook
+ * @property {RegExp | string} pattern
+ * @property {(data: string, filename: string) => string | Promise<string>} callback
+ */
+
+/**
+ * Registry of filesystem hooks.
+ * @type {{ [type: string]: Hook[] }}
+ */
+var hooksRegistry;
+
+/**
+ * Clear all register hooks, returns function to restore them.
+ * Useful for testing.
+ */
+exports.clearHooks = function () {
+    var oldHooks = hooksRegistry;
+    hooksRegistry = {
+        write: [],
+    };
+    return () => { hooksRegistry = oldHooks; };
+};
+
+// Initialize hooks
+exports.clearHooks();
+
+/**
+ * Call any hooks of the specified type (e.g. "write"), in order of registration,
+ * when they match the filename. First hook is called with data, subsequent hooks
+ * are called with result of previous hook.
+ * If no hooks match, the data is returned as-is (in a Promise).
+ *
+ * Filename is first made relative to args.rootdir, to make matching independent
+ * of location of this package.
+ *
+ * @param type {string} Type of hook (e.g. "write")
+ * @param filename {string} Filename to match against
+ * @param data {string} Data to be passed to hook
+ * @return {Promise<string>} Transformed data
+ */
+exports.callHooks = function (type, filename, data) {
+    const hooks = hooksRegistry[type];
+    if (!hooks) {
+        throw new Error("unknown hook type " + type);
+    }
+
+    // Make filename a relative path, e.g. "data/scores.json"
+    filename = path.relative(args.rootdir, filename);
+
+    // Start with source data
+    const initial = Promise.resolve(data);
+
+    // Run each hook consecutively
+    return hooks.reduce((intermediate, hook) => {
+        const pattern = hook.pattern;
+        const isMatch = typeof pattern === "string" ? pattern === filename : pattern.test(filename);
+        if (!isMatch) {
+            return intermediate;
+        }
+        // It matches: add another transform to the chain
+        return intermediate.then((data) => {
+            const transformed = hook.callback(data, filename);
+            if (typeof transformed !== "string" && (typeof transformed !== "object" || typeof transformed.then !== "function")) {
+                throw new Error("hook for pattern " + hook.pattern.toString() + " returned invalid data");
+            }
+            return transformed;
+        });
+    }, initial);
+}
+
+/**
+ * Register a hook to be called when operating on a file.
+ *
+ * Usage example:
+ *   registerHook('write', /^foo\.txt$/, (data) => data.toUpperCase());
+ * or
+ *   registerHook('write', 'foo.txt', (data) => data.toUpperCase());
+ *
+ * Then, writeFile('foo.txt', 'something') will write SOMETHING instead.
+ *
+ * Hooks are called in order of registration, and subsequent hooks will
+ * receive the output of the previous hook(s).
+ *
+ * Before matching and calling hooks, any filename is first made relative
+ * to the package's root dir (i.e. `args.rootdir`).
+ *
+ * @param type {string} Type of hook (e.g. "write")
+ * @param pattern {RegExp | string} Matched against filename relative to root dir (e.g. /^data\/scores\.json$/)
+ * @param callback {(data: string, filename: string) => string | Promise<string>}
+ *            Called when pattern matches filename.
+ *            Receives the data to be written (utf-8) and filename, is expected to return (promise for) optionally
+ *            modified file data. It is an error to return nothing.
+ * @return void
+ */
+exports.registerHook = function (type, pattern, callback) {
+    const hooks = hooksRegistry[type];
+    if (!hooks) {
+        throw new Error("unknown hook type " + type);
+    }
+    hooks.push({
+        pattern,
+        callback
+    });
+};
+
 exports.readFile = function(file) {
     file = exports.resolve(file);
 
--- server_test/file_systemSpec.js
+++ server_test/file_systemSpec.js
@@ -5,6 +5,11 @@ const mockFs = require("mock-fs");
 const file_system = require("../server_modules/file_system");
 
 describe("file_system", () => {
+    // Create empty hooks before tests, then restore original ones after tests
+    let restoreHooks;
+    beforeEach(() => restoreHooks = file_system.clearHooks());
+    afterEach(() => restoreHooks());
+
     afterEach(() => mockFs.restore());
 
     describe("resolve", () => {
@@ -14,6 +19,95 @@ describe("file_system", () => {
 
             const rootdir = path.dirname(packageJson);
             expect(file_system.resolve("foo.txt")).toBe(path.resolve(rootdir, "foo.txt"));
+        });
+    });
+
+    describe("registerHook", () => {
+        it("allows registering hook using string", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHING");
+        });
+
+        it("allows registering hook using regex", async () => {
+            file_system.registerHook("write", /^foo\.txt$/, (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHING");
+        });
+
+        it("throws for unknown hook types", () => {
+            expect(() => file_system.registerHook("foo", "foo.txt", (data) => data)).toThrow();
+        });
+    });
+
+    describe("callHooks", () => {
+        it("returns raw data without hooks", async () => {
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("something");
+        });
+
+        it("matches hooks using string, match", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHING");
+        });
+
+        it("matches hooks using string, no match", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "fooXtxt", "something");
+            expect(result).toBe("something");
+        });
+
+        it("matches hooks using regex, match", async () => {
+            file_system.registerHook("write", /^foo\.txt$/, (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHING");
+        });
+
+        it("matches hooks using regex, no match", async () => {
+            file_system.registerHook("write", /^foo\.txt$/, (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "fooXtxt", "something");
+            expect(result).toBe("something");
+        });
+
+        it("supports synchronous transforms", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => data + data);
+            file_system.registerHook("write", "foo.txt", (data) => data.toUpperCase());
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHINGSOMETHING");
+        });
+
+        it("supports asynchronous transforms", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => Promise.resolve(data + data));
+            file_system.registerHook("write", "foo.txt", (data) => Promise.resolve(data.toUpperCase()));
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("SOMETHINGSOMETHING");
+        });
+
+        it("throws when returning nothing from hook", async () => {
+            file_system.registerHook("write", "foo.txt", (data) => { /* no op */ });
+            await file_system.callHooks("write", "foo.txt", "something").then(
+                () => fail("should throw"),
+                (e) => expect(e instanceof Error).toBe(true)
+            );
+        });
+
+        it("matches against relative paths", async () => {
+            file_system.registerHook("write", "data/foo.txt", (data) => data.toUpperCase());
+            const fullPath = file_system.resolve("data/foo.txt");
+            const result = await file_system.callHooks("write", fullPath, "something");
+            expect(result).toBe("SOMETHING");
+        });
+    });
+
+    describe("clearHooks", () => {
+        it("allows to clear and restore hooks", async () => {
+            file_system.registerHook("write", "foo.txt", () => "abc");
+            const restore = file_system.clearHooks();
+            file_system.registerHook("write", "foo.txt", () => "def");
+            restore();
+            const result = await file_system.callHooks("write", "foo.txt", "something");
+            expect(result).toBe("abc");
         });
     });
 
