108e682f17761b2cbc03d97d0a872e7a222ce4c4
Idan Stark <idanstark42@gmail.com>
2017-10-20 00:35:51 +0300

    log name

--- src/js/services/ng-log.js
+++ src/js/services/ng-log.js
@@ -1,8 +1,8 @@
-define('services/ng-session',[
+define('services/ng-log',[
     'services/ng-services',
 ], function(module) {
 
-    return module.service('$session', [
+    return module.service('$log', [
         '$http',
         function($http) {
 
