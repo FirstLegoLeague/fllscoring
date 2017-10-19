213828f3ace272d601822641d32396a269dc2d76
ThinkRedstone <ittai11@gmail.com>
2017-10-20 02:35:30 +0300

    removed confusing addedResponses array

--- spec/mocks/httpMock.js
+++ spec/mocks/httpMock.js
@@ -1,20 +1,19 @@
 var createHttpMock = function(responses) {
-    var addedResponses = [];
     var mock = {
         addResponse: function (method, url, data) {
             responses[method][url] = data;
-            addedResponses.push({method: method, url: url})
         },
 
         resetResponses: function () {
-            addedResponses.forEach(v => delete responses[v.method][v.url])
-            addedResponses = [];
+            responses = originalResponses;
         }
     };
 
     if(!responses) {
         responses = {};
     }
+    var originalResponses = angular.copy(responses);
+
 
     ['get','post','delete','put','patch'].forEach(function(method) {
         mock[method] = jasmine.createSpy(method).and.callFake(function(url) {
