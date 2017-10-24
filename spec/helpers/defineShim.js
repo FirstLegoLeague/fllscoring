68af04494b00ab197a360c2ad821de3b1d25f9a4
Martin Poelstra <martin@beryllium.net>
2017-10-24 22:08:44 +0200

    spec/defineShim: Support CommonJS / UMD style, disallow duplicate module names.

--- spec/helpers/defineShim.js
+++ spec/helpers/defineShim.js
@@ -7,6 +7,35 @@
 !function() {
     var store = {};
 
+    /**
+     * Resolve the relative path of the require'd module
+     * to the current module's basename.
+     * e.g. if name = "common/foo" and dep = "./bar",
+     * result will be "common/bar".
+     */
+    function resolveRelativeDependency(name, dep) {
+        var base = name.split("/");
+        var relative = dep.split("/");
+        base.pop(); // take the 'filename' off, leaving the 'current folder'
+        if (relative[0] !== "." && relative[0] !== "..") {
+            throw new Error("invalid dependency '" + dep + "' in module '" + name + "': relative path expected");
+        }
+        while (relative.length > 0) {
+            var part = relative.shift();
+            if (part === "" || part === ".") {
+                continue;
+            } else if (part === "..") {
+                if (base.length === 0) {
+                    throw new Error("invalid dependency '" + dep + "' in module '" + name + "': cannot go up this far");
+                }
+                base.pop();
+            } else {
+                base.push(part);
+            }
+        }
+        return base.join("/");
+    }
+
     function resolve(name, deps, cache) {
         if (typeof name !== "string") {
             throw new Error("factory(): invalid module name: " + name);
@@ -20,21 +49,41 @@
             throw new Error("factory(): module " + name + " is not yet define()'ed");
         }
 
-        // Resolve dependencies of this module to either the
-        // passed in custom dependencies, or the 'real' ones.
-        var resolvedDeps = store[name].deps.map(function(dep) {
-            // Use custom deps, if passed
-            if (deps && dep in deps) {
-                return deps[dep];
-            }
-            // Use the 'real' module instead, but make sure that if
-            // that real module uses a dependency that we want to
-            // override, we use that instead.
-            return resolve(dep, deps, cache);
-        });
+        if (store[name].deps !== undefined) {
+            // 'Normal' AMD module, as used in all of our front-end code.
 
-        // Instantiate module
-        cache[name] = store[name].factory.apply(this, resolvedDeps);
+            // Resolve dependencies of this module to either the
+            // passed in custom dependencies, or the 'real' ones.
+            var resolvedDeps = store[name].deps.map(function(dep) {
+                // Use custom deps, if passed
+                if (deps && dep in deps) {
+                    return deps[dep];
+                }
+                // Use the 'real' module instead, but make sure that if
+                // that real module uses a dependency that we want to
+                // override, we use that instead.
+                return resolve(dep, deps, cache);
+            });
+
+            // Instantiate module
+            cache[name] = store[name].factory.apply(this, resolvedDeps);
+        } else {
+            // UMD style module, as used in our common (client+server) code.
+
+            function require(dep) {
+                if (typeof dep !== "string" || dep === "") {
+                    throw new Error("invalid dependency '" + dep + "' in module '" + name + "'");
+                }
+                return resolve(resolveRelativeDependency(name, dep), deps, cache);
+            }
+            var module = { exports: {} };
+            var exports = module.exports;
+
+            // Instantiate module
+            store[name].factory.call(this, require, exports, module);
+            cache[name] = module.exports;
+        }
+
         return cache[name];
     }
 
@@ -70,14 +119,31 @@
      *
      * Shim for RequireJS define functionality.
      *
-     * @see documentation of `factory()` for usage and caveats.
+     * @see documentation of `factory()` for caveats.
+     *
+     * @param name {string} Name of module, e.g. "services/ng-scores"
+     * @param deps {Array<string>} Optional array of dependencies. @see `factory`
+     * @param factory {Function} When deps are given, function receiving deps as args, returning the module, when
+     *            no deps are given, function receiving `require`, `exports`, `module`, returning nothing
+     *            (setting its exports through `exports` or `module.exports`).
      */
     this.define = function(name, deps, factory) {
         if (typeof name !== "string") {
             throw new Error("factory(): invalid module name: " + name);
         }
-        if (!Array.isArray(deps)) {
-            throw new Error("factory(): invalid dependencies (expected an array): " + deps);
+        if (store[name]) {
+            throw new Error("factory(): duplicate module name: " + name);
+        }
+        if (typeof deps === "function") {
+            // This is a UMD-style call, which only passes the module name
+            // and a factory function which expects to receive
+            // `require`, `exports`, and `module`.
+            factory = deps;
+            deps = undefined;
+        } else {
+            if (!Array.isArray(deps)) {
+                throw new Error("factory(): invalid dependencies (expected an array): " + deps);
+            }
         }
         //store factories in a global store
         store[name] = {
